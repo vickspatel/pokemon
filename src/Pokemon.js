0ca28258603c6c8a0bdafdd55658255a331fd94f
vickspatel <pvikas_03@yahoo.com>
2019-07-31 10:30:32 -0400

    fix id for image

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 export class Pokemon extends Component{
     render(){
         const {pokemon} = this.props;
-        const id = pokemon.url.charAt(pokemon.url.length - 2);
+        const urlArray = pokemon.url.split('/');
+        const id = urlArray[urlArray.length - 2];
         return (
             <div className="card">
                 <div className="card-header bg-success d-flex">
@@ -12,7 +13,7 @@ export class Pokemon extends Component{
                 </div>
                 <div className="card-body">
                     <div className="card-img">
-                        <img src={`http://pokeapi.co/media/img/${id}.png`} alt=""/>
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt=""/>
                     </div>
                 </div>
             </div>
